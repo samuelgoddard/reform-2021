1b95f3db38b82ba17d7514f1b4c596b4b3f45568
Sam Goddard <hello@samgoddard.co.uk>
2021-09-23 09:14:05 +0100

    preview mode

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,8 @@ export default function App({ Component, pageProps }) {
 
       {/* <div className="w-full h-screen absolute top-0 left-0 right-0 bottom-0 texture-overlay z-0 pointer-events-none"></div> */}
 
+      { pageProps.preview && <><div className={'fixed bottom-0 w-full p-2 bg-red-500 bg-opacity-100 text-white justify-center flex z-50 uppercase font-medium'}>! Preview Mode Enabled - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a> !</div></> }
+
       <Header noLogo={ router.pathname === '/' ? true : false }/>
 
       <AnimatePresence exitBeforeEnter>
